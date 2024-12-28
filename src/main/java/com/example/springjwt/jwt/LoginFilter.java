97ee40ca2927691543c3ea2efbc9343fb4822f64
kisrin4319 <kisrin4319@gmail.com>
2024-12-28 18:24:45 +0900

    feat: 로그인 검증 로직 구현

--- src/main/java/com/example/springjwt/jwt/LoginFilter.java
+++ src/main/java/com/example/springjwt/jwt/LoginFilter.java
@@ -1,6 +1,9 @@
 package com.example.springjwt.jwt;
 
+import com.example.springjwt.dto.LoginDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,6 +11,10 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -20,11 +27,20 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
 
+        LoginDTO loginDTO = new LoginDTO();
 
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            ServletInputStream inputStream = request.getInputStream();
+            String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+            loginDTO = objectMapper.readValue(messageBody, LoginDTO.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         //클라이언트 요청에서 Username, Password 값 추출
-        String username = obtainUsername(request);
-        String password = obtainPassword(request);
+        String username = loginDTO.getUsername();
+        String password = loginDTO.getPassword();
 
         //스프링 시큐리티에서 username과 password를 검증하기 위해서는 token에 담아야 함
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password,null);
@@ -38,11 +54,15 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
 
+        System.out.println("successful authentication");
+
     }
 
     //로그인 실패시 실행하는 메소드
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
+
+        System.out.println("unsuccessful authentication");
 
     }
 }
--- src/main/java/com/example/springjwt/repository/UserRepository.java
+++ src/main/java/com/example/springjwt/repository/UserRepository.java
@@ -8,4 +8,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
 
     Boolean existsByUsername(String username);
+
+    //username을 받아 DB 테이블에서 회원을 조회하는 메소드 작성
+    User findByUsername(String username);
 }
